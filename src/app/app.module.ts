857ebb71e3fb73caeca79b597f1a0f0046b94875
Marcelo Suarez <marcelo.suarez.v@gmail.com>
2023-10-16 17:49:37 +0200

    feat: components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { TrainingComponent } from './components/training/training.component';
+import { NewTrainingComponent } from './components/training/new-training/new-training.component';
+import { LastTrainingsComponent } from './components/training/last-trainings/last-trainings.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignupComponent, LoginComponent, TrainingComponent, NewTrainingComponent, LastTrainingsComponent, CurrentTrainingComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/training/last-trainings/last-trainings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-last-trainings',
+  templateUrl: './last-trainings.component.html',
+  styleUrls: ['./last-trainings.component.css']
+})
+export class LastTrainingsComponent {
+
+}
