d0da9290670bc97ad0b21248c332c351cd8dab3a
Marcelo Suarez <marcelo.suarez@clearpeaks.com>
2023-10-18 18:00:12 +0200

    feat: training service createdç

---
+++ src/app/services/training.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Exercise } from '../interfaces/exercise';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TrainingService {
+  availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 100, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
+  ];
+
+  constructor() {}
+}
